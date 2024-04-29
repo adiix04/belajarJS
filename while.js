49e52158e4ee1b06497c8b789169ca090ec46dfe
Adi <adisocialmediax04@gmail.com>
2024-04-29 16:23:23 +0700

    latihan hari ke-2

---
+++ while.js
@@ -0,0 +1,13 @@
+// dihentikan user
+var ulang = true;
+while(ulang) {
+    console.log('hello world!');
+    ulang = confirm('lagi?');
+}
+
+// dihentikan program
+var i = 1;
+while (i<=5) {
+    console.log('Hello World!' + i)
+    i++;
+}
