48625446141513875a0c41c2503c30252c38c642
Adi <adisocialmediax04@gmail.com>
2024-05-02 12:12:12 +0700

    latihan hari ke-4

---
+++ ifelse.js
@@ -0,0 +1,6 @@
+var angka = prompt('masukan angka:');
+if(angka %2===0) {
+    alert('Angka genap')
+} else {
+    alert('Angka Ganjil')
+}
---
+++ latihanifelse.js
@@ -0,0 +1,10 @@
+var jmlBus=10;
+var busBeroperasi=6;
+
+for(noBus=1; noBus<=jmlBus; noBus++ ) {
+    if(noBus<=6) {
+        console.log('Bus no. '+noBus+' beroperasi dengan baik')
+    } else {
+        console.log('Bus no. '+noBus+' sedang tidak beropreasi.')
+    }
+} 
