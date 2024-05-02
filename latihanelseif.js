7d6d9f54a0ad573e84f8621615b43bdd311cded0
Adi <adisocialmediax04@gmail.com>
2024-05-02 13:26:20 +0700

    latihan hari ke-4 latihan kasus komplek

--- latihanelseif.js
+++ latihanelseif.js
@@ -2,9 +2,13 @@ var jmlBecak = 10;
 var becakBeroperasi = 6;
 
 for(noBecak=1; noBecak<=jmlBecak; noBecak++) {
-    if(noBecak<=becakBeroperasi){
+    if(noBecak<=becakBeroperasi && noBecak !== 5){ // && jika kondisi salahsatunya false maka semuanya false
         console.log('Becak no. '+noBecak+' sedang beroperasi dengan baik')
-    } else if(noBecak==8) {
+    } 
+    else if (noBecak===5){
+        console.log('Becak no. '+noBecak+ ' sedang lembur')
+    }
+    else if(noBecak===8 || noBecak===10) { // || jika kondisi salahstunya false akan tetap dikerjakan
         console.log('Becak no. '+noBecak+ ' tiguling!')
     } else{
         console.log('Becak no. '+noBecak+' sedang tidak beroperasi')
